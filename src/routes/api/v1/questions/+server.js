0bc7351afe884ce6b4e8000a60d48d8ddea949a5
joostf <j.faber@hva.nl>
2023-03-22 10:31:39 +0100

    Fixed questions endpoint

--- src/routes/api/v1/questions/+server.js
+++ src/routes/api/v1/questions/+server.js
@@ -1,110 +1,35 @@
+import { hygraphOnSteroids } from '$lib/server/hygraph'
 import { gql } from 'graphql-request'
-import { hygraph, hygraphOnSteroids } from '$lib/server/hygraph'
 import { responseInit } from '$lib/server/responseInit'
 
 export async function GET({ url }) {
-  const id = url.searchParams.get('id') ?? ''
-  const query = queryGetAnswers()
-  const data = await hygraphOnSteroids.request(query, { id })
-  
+  const first = Number(url.searchParams.get('first') ?? 5)
+  const skip = Number(url.searchParams.get('skip') ?? 0)
+  const direction = url.searchParams.get('direction') === 'ASC' ? 'ASC' : 'DESC'
+  const orderBy = (url.searchParams.get('orderBy') ?? 'publishedAt') + '_' + direction
+  const query = queryGetQuestions()
+  const data = await hygraphOnSteroids.request(query, { first, skip, orderBy })
+
   return new Response(JSON.stringify(data), responseInit)
 }
 
-function queryGetAnswers() {
+function queryGetQuestions() {
   return gql`
-    query getAnswers($id: ID!) {
-      answers(where: { player: { id: $id } }) {
-        question {
-          id
-          title
+    query getQuestions($first: Int, $skip: Int, $orderBy: QuestionOrderByInput) {
+      questions(first: $first, skip: $skip, orderBy: $orderBy) {
+        id
+        title
+        answers {
+            content
         }
-        content
-        player {
-          id
-          name
+      }
+      questionsConnection {
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+          pageSize
         }
       }
     }
   `
 }
-
-export async function POST({ request }) {
-  const requestData = await request.json()
-  const errors = []
-
-  // Controleer de request data op juistheid
-  if (!requestData.questionTitle || typeof requestData.title !== 'string') {
-    errors.push({ field: 'questionTitle', message: 'questionTitle should exist and have a string value' })
-  }
-  if (!requestData.content || typeof requestData.content !== 'string') {
-    errors.push({ field: 'content', message: 'content should exist and have a string value' })
-  }
-  if (!requestData.playerId) {
-    errors.push({ field: 'playerId', message: 'playerId should exist' })
-  }
-  
-
-  // Als we hier al errors hebben in de form data sturen we die terug
-  if (errors.length > 0) {
-    return new Response(
-      JSON.stringify({
-        errors: errors,
-      }),
-      {status: 400}
-    )
-  }
-
-  // Bereid de mutatie voor
-  const mutation = gql`
-    mutation createAswer($questionTitle: String!, $content: String!, $playerId: ID!) {
-      createAnswer(data: { questionTitle: $questionTitle, answer: $content, player: { connect: { id: $playerId } } }) {
-        id
-      }
-    }
-  `
-  // Bereid publiceren voor
-  const publication = gql`
-    mutation publishAnswers($id: ID!) {
-      publishAnswer(where: { id: $id }, to: PUBLISHED) {
-        id
-      }
-    }
-  `
-
-  // Voer de mutatie uit
-  const data = await hygraph
-    .request(mutation, { ...requestData })
-    // Stuur de response met created id door
-    .then((data) => {
-      return (
-        hygraph
-          // Voer de publicatie uit met created id
-          .request(publication, { id: data.createAnswer.id ?? null })
-          // Vang fouten af bij het publiceren
-          .catch((error) => {
-            errors.push({ field: 'HyGraph', message: error })
-          })
-      )
-    })
-    // Vang fouten af bij de mutatie
-    .catch((error) => {
-      errors.push({ field: 'HyGraph', message: error })
-    })
-
-  if (errors.length > 0) {
-    return new Response(
-      JSON.stringify({
-        errors: errors,
-      }),
-      {status: 400}
-    )
-  }
-
-  return new Response(
-    JSON.stringify({
-      data: data && data.publishQuestion,
-    }),
-    responseInit
-  )
-}
-
--- src/routes/docs/questions/+page.svelte
+++ src/routes/docs/questions/+page.svelte
@@ -2,6 +2,67 @@
 
 <h3>GET</h3>
 
+<p>
+	Een gepagineerd overzicht van alle <em>questions</em> met een aantal in te stellen
+	opties welke als url parameter meegestuurd kunnen worden. Standaard worden de laatst toegevoegde
+	tien <em>questions</em> ingeladen maar dit aantal, het aantal <em>questions</em> dat geskipt worden, de
+	volgorde en de kolom waarop gesorteerd wordt kunnen worden ingesteld.
+</p>
+
+<h4>Url parameters:</h4>
 <ul>
-  <li><a href="/api/v1/questions?id=clfcp0hys0mxo0buufcn33obp">/api/v1/questions/?id=clfcp0hys0mxo0buufcn33obp</a></li>
+	<li>
+		<strong>first</strong>, een getal tussen 0-100 waarmee het aantal op te halen
+		<em>questions</em> wordt ingesteld.
+	</li>
+	<li>
+		<strong>skip</strong>, een getal waarmee een aantal <em>questions</em> in de selectie worden overgeslagen.
+		Dit is handig voor het instellen van paginering.
+	</li>
+	<li>
+		<strong>direction</strong>, bepaalt de volgorde van de selectie. Standaard is DESC (aflopend)
+		maar je kunt deze parameter ASC (oplopend) meegeven om de richting te veranderen.
+	</li>
+	<li>
+		<strong>orderBy</strong>, bepaalt de sortering van de selectie. Standaard is
+		<em>publishedAt</em>, de datum dat het record is toegevoegd. Alle eigenschappen van
+		<em>questions</em> kunnen gebruikt worden als sortering.
+	</li>
 </ul>
+
+<h4>Voorbeeld Requests:</h4>
+<ul>
+	<li>
+		<a href="/api/v1/questions">/api/v1/questions</a>, selecteer 10 <em>questions</em>.
+	</li>
+	<li>
+		<a href="/api/v1/questions?first=7">/api/v1/questions?first=7</a> selecteer 7 <em>questions</em>.
+	</li>
+	<li>
+		<a href="/api/v1/questions?first=20&skip=20">/api/v1/questions?first=20&skip=20</a>, selecteer 20
+		<em>questions</em> en sla de eerste 20 over. Met andere woorden, selecteer pagina 2.
+	</li>
+	<li>
+		<a href="/api/v1/questions?first=20&direction=ASC">/api/v1/questions?first=20&direction=ASC</a>,
+		selecteer 20 <em>questions</em> in oplopende volgorde.
+	</li>
+	<li>
+		<a href="/api/v1/questions?orderBy=name">/api/v1/questions?orderBy=name</a>, selecteer 10
+		<em>questions</em>
+		gesorteerd op <em>name</em>.
+	</li>
+</ul>
+
+<style>
+	h4,
+	ul {
+		margin-top: 1rem;
+	}
+	ul {
+		padding: 0;
+		list-style: none;
+	}
+	li {
+		margin-bottom: 0.4rem;
+	}
+</style>
