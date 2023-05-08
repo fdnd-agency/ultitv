ff8ffa8d69613b7832bacdb7555019896c9c622a
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-05-08 11:42:15 +0200

    Added POST for team

--- src/routes/api/v2/teams/+server.js
+++ src/routes/api/v2/teams/+server.js
@@ -76,6 +76,70 @@ function queryGetTeams(id){
             }
         `
     }
+}
 
+export async function POST({ request }){
+    const requestData = await request.json()
+    const errors = []
+
+    // Mutation query for adding team
+    const mutation = gql`
+        mutation createTeam($name: String!, $country: String!, $seeding: Int!, iso2: String, iso3: String, olympicCode: String){
+            createTeam(
+                data: {
+                    name: $name
+                    country: $country
+                    seeding: $seeding
+                    iso2: $iso2
+                    iso3: $iso3
+                    olympicCode: $olympicCode
+                }
+            ){
+                id
+            }
+        }
+    `
+
+    // Mutation for publication
+    const publication = gql`
+        mutation publishTeam($id: ID!){
+            publishTeam(where: { id: $id }, to: PUBLISHED){
+                id
+            }
+        }
+    `
+
+    // Execute mutation
+    const data = await hygraph.request(mutation, {...requestData})
+        .then((data) => {
+            return (
+                // Execute publication
+                hygraph.request(publication, { id: data.createTeam.id ?? null })
+                // Catch error if publication fails
+                .catch((error) => {
+                    error.push({ field: 'HyGraph', message: error})
+                })
+            )
+        })
+        // Catch error if mutation fails
+        .catch((error) => {
+            error.push({ field: 'HyGraph', message: error})
+        })
     
+    // Check error length
+    if (errors.length > 0) {
+        return new Response(
+            JSON.stringify({ 
+                errors: errors, 
+            }),
+            { status: 400}
+        )
+    }
+
+    return new Response(
+        JSON.stringify({
+            data: data && data.publishTeam,
+        }),
+        responseInit
+    )
 }
